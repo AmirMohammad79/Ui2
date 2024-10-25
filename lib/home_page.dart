870b2f0ef4e9f2ed058958262a47a18dfeecdce1
amir <a.abdpour0881@gmail.com>
2024-10-25 22:26:52 +0330

    add app bar on home page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -10,6 +10,25 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+
+      body: SafeArea(
+        child: Column(children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0),
+            child: Row(
+              children: [
+                Image.asset('images/menu1.png' , height: 25,),
+                const Spacer(),
+                Image.asset('images/search.png' , height: 25,),
+                const SizedBox(width: 15,),
+                Image.asset('images/shopping bag.png' , height: 25,),
+              ],
+            ),
+          )
+        ],),
+      ),
+    );
   }
 }
